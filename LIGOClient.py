a0c4d3226c4f0ccea734bb0f19b9c63d3f713e49
Jeremy Heyl <heyl@phas.ubc.ca>
2015-11-07 07:45:01 -0800

    add licence terms

--- LIGOClient.py
+++ LIGOClient.py
@@ -14,6 +14,19 @@
 # Downloads a probability map from the Grace Database and
 # then passes it along to ObsPlan to generate an observation plan.
 #
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from argparse import ArgumentParser
 import math as mt
 import numpy as np
@@ -53,7 +66,7 @@ def _parse_command_line_arguments():
     """
     parser = ArgumentParser(
         description=(
-            'Command line generating an observing plan from a LIGO/Virgo GraceID (with an optional galaxy map too)'
+            'Command-line tool to generate an observing plan from a LIGO/Virgo GraceID (with an optional galaxy map too)'
         ),
     )
     parser.add_argument(
--- ObsPlan.py
+++ ObsPlan.py
@@ -33,6 +33,19 @@
 #
 # Questions: heyl@phas.ubc.ca
 #
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 
 from argparse import ArgumentParser
@@ -192,7 +205,7 @@ def _parse_command_line_arguments():
     """
     parser = ArgumentParser(
         description=(
-            'Command line generating an observing plan from a LIGO/Virgo probability map (with an optional galaxy map too)'
+            'Command-line tool to generate an observing plan from a LIGO/Virgo probability map (with an optional galaxy map too)'
         ),
     )
     parser.add_argument(
