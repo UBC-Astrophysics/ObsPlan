c0764b20054ff081df37e88aea29d570d5d0a9fa
Jeremy Heyl <heyl@phas.ubc.ca>
2016-04-06 20:13:16 -0700

    various changes

--- calcplot.sh
+++ calcplot.sh
@@ -7,24 +7,28 @@ if [ $# -lt 2 ]; then
 fi
 out=${1/.fits.gz/}
 galmap=${2/.fits.gz/}
-for i in 16 32 64 128 ; do
+for i in 256 ; do
     python ObsPlan.py --gal-map $2 --cumprob 0.99 --no-savefigures ${1} $i > ${out}_${galmap}_${i}.dat
     python ObsPlan.py --gal-map ${galmap}_smoothed.fits.gz --cumprob 0.99 --no-savefigures ${1} $i > ${out}_${galmap}_s_${i}.dat
     python ObsPlan.py  --cumprob 0.99 --no-savefigures ${1} $i > ${out}_raw_${i}.dat
 done
 ctioga2 --name ${out}_${galmap} --ylog \
+	-c blue  ${out}_${galmap}_256.dat@5:6  \
+	-c green ${out}_${galmap}_s_256.dat@5:6  \
+	-c red   ${out}_raw_256.dat@5:6  \
+    --line-style dashes \
 	-c blue  ${out}_${galmap}_128.dat@5:6  \
 	-c green ${out}_${galmap}_s_128.dat@5:6  \
 	-c red   ${out}_raw_128.dat@5:6  \
-    --line-style dashes \
+    --line-style dots \
     -c blue  ${out}_${galmap}_64.dat@5:6  \
     -c green ${out}_${galmap}_s_64.dat@5:6  \
     -c red   ${out}_raw_64.dat@5:6  \
-    --line-style dots \
+    --line-style solid \
   -c blue   ${out}_${galmap}_32.dat@5:6  \
   -c green  ${out}_${galmap}_s_32.dat@5:6  \
   -c red    ${out}_raw_32.dat@5:6  \
-    --line-style solid \
+    --line-style dashes \
 	-c blue  ${out}_${galmap}_16.dat@5:6  \
 	-c green ${out}_${galmap}_s_16.dat@5:6  \
 	-c red   ${out}_raw_16.dat@5:6  \
